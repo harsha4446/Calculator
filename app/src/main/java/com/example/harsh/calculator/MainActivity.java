be060ee0b0d1eca48d93f64558a392476c378ecd
Harsha Bandi <harsha44467@gmail.com>
2017-02-06 23:54:57 +0530

    incremental, fill in all the button operations. (getText,parse,*10+num,settext)

--- app/src/main/java/com/example/harsh/calculator/MainActivity.java
+++ app/src/main/java/com/example/harsh/calculator/MainActivity.java
@@ -61,7 +61,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         switch(v.getId()){
             case R.id.num0:
-
+                            break;
+            case R.id.num1:
+                            break;
         }
 
     }
